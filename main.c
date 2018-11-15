b604e6e255f5d5d183e7ab9c96ea760337126af0
Divyansh Bhardwaj <divyanshb22@gmail.com>
2018-11-15 16:02:39 +0530

    add main.c to the project

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+*   FileName    :   main.c
+*   Purpose     :   To house the main function of Tic Tac Toe Project
+*   Author      :   Divyansh Bhardwaj, Technical Trainer, GLAU
+*   Operating System    :   Ubuntu 18.10, 64 Bit
+*   Compiler    :   GCC 8.2.0 x86_64 GNU
+*/
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    system("clear");    // this statement is OS dependent
+    //  for a Windows OS try,
+    //  system("cls");
+    return 0;
+}
