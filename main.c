48b468db954a9c6ba0f0b9ca6f226a5bc29292fd
Divyansh Bhardwaj <divyanshb22@gmail.com>
2018-11-15 16:11:24 +0530

    add executables for main.c
    print the welcome message to the user's screen;
    print 3x3 boxes for the tic-tac-toe board

--- main.c
+++ main.c
@@ -7,10 +7,40 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
-int main()
+
+void show_board(void);  //  this function will show the current board to the user.
+
+int main(void)
 {
     system("clear");    // this statement is OS dependent
-    //  for a Windows OS try,
+    //  this only works on LInux
+    //  for Windows OS try,
     //  system("cls");
+
+    //  print the welcome message to the user
+    printf("\t-\t-\tWelcome to GLA's Tic-Tac-Toe\t-\t-\t\n");
+
+    //  show the board to the user
+    show_board();
+
     return 0;
+}
+
+void show_board(void)
+{
+    char user_choice = ' '; //  to hold the char of user's choice ['O' or 'X']
+    int i = 0, j = 0;  //  variables for loop
+    //  the tic-tac-toe board looks like a 3x3 matrix
+
+    //  I'm going to print 3 boxes with allowance for a 3-width char
+    for ( i = 0 ; i < 3 ; i++ )
+    {
+        for ( j = 0 ; j < 3 ; j++ )
+        {
+            printf("|\t%3c\t|", user_choice);
+        }
+        printf("\n");
+    }
+
+
 }
